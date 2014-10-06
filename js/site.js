c659dc5de270ff5e7a24ff58749e1fe5a86ed871
Dylan Vaughn <dylancvaughn@gmail.com>
2014-10-06 16:55:40 -0700

    adding contact us form / section

--- js/site.js
+++ js/site.js
@@ -30,4 +30,21 @@ $(document).ready(function() {
     'customCallback': renderTweets
   };
   twitterFetcher.fetch(twitterConfig);
+
+  // http://www.jquerybyexample.net/2012/06/get-url-parameters-using-jquery.html
+  function GetURLParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+      var sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] == sParam) {
+        return sParameterName[1];
+      }
+    }
+  }
+
+  if (GetURLParameter('contacted') == '1') {
+    $('#contacted-thanks').show();
+  }
+
 });
